47fabbcbf88381621ae39cc56ac1c56a6dff820e
itsuki-chan <82200440+Adiixyz@users.noreply.github.com>
2021-09-14 06:33:20 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,7 @@
 function handler(m) {
-  this.sendContact(m.chat, '60199782326', 'Owner Itsuki', m)
-  this.fakeReply(m.chat, `Ini owner gw ya ${this.getName(m.sender}`, '0@s.whatsapp.net', 'OWNER BOT', 'status@broadcast')
+  this.sendContact(m.chat, '60199782326', 'itsuki-chan', m)
+  this.fakeReply(m.chat, `Ini owner gw ya bwang`, '0@s.whatsapp.net', 'OWNER BOT', 'status@broadcast')
+  m.reply('nih nomor whatsapp @0 :v')
 }
 handler.help = ['owner', 'creator', 'itsukibotowner']
 handler.tags = ['info']
