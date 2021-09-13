85ef76705c8742078d44989c7d5385c658b61108
itsuki-chan <82200440+Adiixyz@users.noreply.github.com>
2021-09-14 06:40:56 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(back)).buffer(), `
+┌〔 Donasi • PULSA 〕
+├ 60199782326
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi')
+donasi bang. biar bot ga ke ban mark kontol:v
+`.trim(), 'donasi lah sayang:v', 'owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
