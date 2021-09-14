0a6abb29edb56a3cae0b776472a833e522173a6d
itsuki-chan <82200440+Adiixyz@users.noreply.github.com>
2021-09-14 14:31:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': 'efe8f3584235b7d4',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://itsuki-api.herokuapp.com': 'YourApikey',
