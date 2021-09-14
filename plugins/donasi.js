517a8ff21643550f129760029c79e0af84e8f30b
itsuki-chan <82200440+Adiixyz@users.noreply.github.com>
2021-09-14 19:49:20 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(back)).buffer(), `
-┌〔 Donasi • PULSA 〕
-├ 60199782326
-└────
-donasi bang. biar bot ga ke ban mark kontol:v
+*DONASI*
+
+PULSA : 60199782326
+SAWERIA : https://saweria.co/itsukichan
+
+donasi bang. biar bot ga ke ban mark :v
 `.trim(), 'donasi lah sayang:v', 'owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
