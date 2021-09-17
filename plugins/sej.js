fb28c6aa74a01b8ab0fc8dde8b3f14694b348226
itsuki-chan <82200440+Adiixyz@users.noreply.github.com>
2021-09-17 12:22:21 +0800

    Update sej.js

--- plugins/sej.js
+++ plugins/sej.js
@@ -1,8 +1,6 @@
 function handler(m) {
   m.reply('pantek')
-  this.send2Button(m.chat, `「 WELCOME 」
-
-To Use welcome`, 'Click Here Bang. Kalo Gak Gw Tembak Lo Aowkaowk', 'WELCOME ON', '.enable welcome', 'WELCOME OFF', '.disable welcome')
+  this.send2Button(m.chat, `「 WELCOME 」`, 'pilih disini', 'WELCOME ON', '.enable welcome', 'WELCOME OFF', '.disable welcome')
 }
 handler.help = ['welcome']
 handler.tags = ['owner']
