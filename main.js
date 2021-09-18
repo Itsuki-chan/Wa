5726696aaa7b544253787b10247a0f71428851d9
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-09-18 11:53:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -54,6 +54,8 @@ if (!opts['test']) setInterval(async () => {
 }, 60 * 1000) // Save every minute
 if (opts['server']) require('./server')(global.conn, PORT)
 
+conn.connectOptions.maxQueryResponseTime = 60_000
+
 if (opts['test']) {
   conn.user = {
     jid: '2219191@s.whatsapp.net',
