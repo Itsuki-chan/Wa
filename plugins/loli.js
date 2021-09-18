f31341e0a209abfc0ff883db3aa27d0506b9d0de
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-09-18 11:03:34 +0800

    Create loli.js

---
+++ plugins/loli.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+
+let handler  = async (m, { conn, args }) => {
+    loli = await fetch(`https://recoders-area.caliph.repl.co/api/loli`)
+    json = await loli.buffer()
+   conn.sendFile(m.chat, json, 'loli', 'Lomlinya kak', m, false)
+}
+handler.help = ['loli']
+handler.tags = ['anime']
+handler.command = /^loli$/i
+
+module.exports = handler
