0d81f425d82f0836f02214eff2e6fb9f249ef536
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-09-22 11:22:31 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -429,9 +429,9 @@ const more = String.fromCharCode(1)
 const readMore = more.repeat(1)
 
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  let h = isNaN(ms) ? 'hh' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? 'mm' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? 'ss' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 function ucapan() {
