1b763e2e2d28ace3a38daaa4bf4cd813d4427508
DanielMalaysia <90729805+DanielMalaysia@users.noreply.github.com>
2021-09-25 18:28:19 +0800

    Create creator.js

---
+++ creator.js
@@ -0,0 +1,11 @@
+function handler(m) {
+  this.sendContact(m.chat, '60196201064', 'Daniel (Owner)', m)
+  this.fakeReply(m.chat, `Ini owner gw ya bwang`, '0@s.whatsapp.net', 'OWNER BOT', 'status@broadcast')
+  m.reply('nih nomor whatsapp @0 :v')
+}
+handler.help = ['owner', 'creator', 'itsukibotowner']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator|itsukibotowner)$/i
+
+module.exports = handler
