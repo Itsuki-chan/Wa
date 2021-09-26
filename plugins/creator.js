57f7fc0ac59f01021c5c9f6b64067fcf27f1e3ca
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-09-26 09:04:43 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,6 @@
 function handler(m) {
-  this.sendContact(m.chat, '60199782326', 'itsuki-chan', m)
-  this.fakeReply(m.chat, `Ini owner gw ya bwang`, '0@s.whatsapp.net', 'OWNER BOT', 'status@broadcast')
-  m.reply('nih nomor whatsapp @0 :v')
+  this.sendContactArray(m.chat, ['60199782326', 'Itsuki-chan', '0', '0', '0', '0', '0', '0'], m)
+  .then(() => this.sendMessage(m.chat, 'Nih kontak ownerku', text, m))
 }
 handler.help = ['owner', 'creator', 'itsukibotowner']
 handler.tags = ['info']
