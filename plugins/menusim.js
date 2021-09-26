503f93e2a24bb7ed592eeb36079269aa523d8599
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-09-26 08:49:47 +0800

    Update menusim.js

--- plugins/menusim.js
+++ plugins/menusim.js
@@ -1,8 +1,11 @@
+const faketroli = {
+                  key: {fromMe: false,participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "0@s.whatsapp.net" } : {})},message: {orderMessage: {itemCount : 2937,status: 1,surface : 1,message: 'Itsuki Botz',orderTitle: 'By Itsuki',thumbnail: back, sellerJid: '0@s.whatsapp.net'}}}
+
 function handler(m) {
   m.reply('Please wait...')
   this.send2Button(m.chat, `*Itsuki Bot*
 
-Please select below`, 'root@Itsukibotz:~#', 'Button Menu', '.?', 'All Menu', '.? all', m)
+Please select below`, 'BETA BOTZ', 'Button Menu', '.?', 'All Menu', '.? all', { quoted: faketroli })
 }
 handler.command = /^(menu|help)$/i
 
