98e9b63854a11d676a02b65783aaaeb24e955b82
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-09-30 22:20:31 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,9 @@
 function handler(m) {
-  this.sendContactArray(m.chat, ['60199782326', 'Itsuki-chan', '0', '0', '0', '0', '0', '0'], m)
-  .then(() => this.sendMessage(m.chat, 'Nih kontak ownerku', text, m))
+  this.sendContactArray(m.chat, ['60199782326', 'CREATOR BOTZ', '994406631981', 'MY BOTZ', '0', '0', '0', '0'], m)
+  this.send2Button(m.chat, `Hello *-* ,
+Itu Owner gw
+
+Mau tanya apa?`, 'BOTZ BY ITSUKI', 'SC BOT', '.sc', 'DONASI', '.donate', m)
 }
 handler.help = ['owner', 'creator', 'itsukibotowner']
 handler.tags = ['info']
