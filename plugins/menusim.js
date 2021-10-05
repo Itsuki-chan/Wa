bf19c3ecb55fc065d1d23d7925998283e984c01a
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-10-05 10:51:38 +0800

    Update menusim.js

--- plugins/menusim.js
+++ plugins/menusim.js
@@ -1,4 +1,5 @@
 function handler(m) {
+  m.reply('Reading *menusim.js*')
   this.send2Button(m.chat, `*Itsuki Bot*
 
 Please select below`, 'BETA BOTZ', 'Button Menu', '.?', 'All Menu', '.? all', m)
