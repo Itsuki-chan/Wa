9032b32630243ae2123287b972d5604430c38148
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-10-09 11:13:49 +0800

    Update menusim.js

--- plugins/menusim.js
+++ plugins/menusim.js
@@ -1,9 +1,20 @@
-function handler(m) {
-  m.reply('Reading *menusim.js*')
-  this.send2Button(m.chat, `*Itsuki Bot*
+let fetch = require('node-fetch')
+let handler = async (m, { conn, usedPrefix }) => conn.send2ButtonLoc(m.chat, await (await fetch(picmenu)).buffer(), `
+*_• ITSUKI BOTZ_*
 
-Please select below`, 'BETA BOTZ', 'Button Menu', '.?', 'All Menu', '.? all', m)
-}
-handler.command = /^(menu|help)$/i
+*- O W N E R*
+wa.me/60199782326
+
+*- R E S T - A P I*
+https://itsuki-api.herokuapp.com
+
+*- S C R I P T  B O T*
+https://github.com/Itsuki-chan/Wa
+
+_Note: Jika button tidak muncul. Ketik ${usedPrefix}command
+`.trim, '_Silahkan pilih dibawah_', 'BUTTON MENU', '.command', 'ALL MENU', '.? all', {quoted:m})
+handler.help = ['menu', 'help']
+handler.tags = ['info']
+handler.command = ['menu', 'help']
 
 module.exports = handler
